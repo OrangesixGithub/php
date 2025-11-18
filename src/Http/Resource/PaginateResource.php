d9c2cceb4883fbfe9077c39a0d608c9bd3948a14
Nandovga123 <nandovga@hotmail.com>
2025-11-18 12:52:18 -0300

    #10 Design Patterns
    Padronização do paginateResource

--- src/Http/Resource/PaginateResource.php
+++ src/Http/Resource/PaginateResource.php
@@ -2,37 +2,49 @@
 
 namespace Orangesix\Http\Resource;
 
-use Illuminate\Database\Query\Builder;
-use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 
 class PaginateResource
 {
     /**
-     * Retorna o array de paginação
+     * Retorna os dados da paginação
+     *
      * @param Builder|EloquentBuilder $query
      * @param string $resource
      * @param Request|null $request
      * @param callable|null $getItens
+     * @param string $action
      * @return array
      */
-    public static function toArray(Builder|EloquentBuilder $query, string $resource, ?Request $request = null, ?callable $getItens = null): array
-    {
+    public static function toArray(
+        Builder|EloquentBuilder $query,
+        string                  $resource,
+        ?Request                $request = null,
+        ?callable               $getItens = null,
+        string                  $action = 'findAll'
+    ): array {
         if (!class_exists($resource) || !is_subclass_of($resource, JsonResource::class)) {
             throw new \InvalidArgumentException("A classe $resource deve ser do tipo de " . JsonResource::class);
         }
 
-        $columns = ['*'];
-        $page = empty($request->page) ? 1 : $request->page;
-        $perPage = empty($request->elements) ? 15 : $request->elements;
+        $request ??= new Request();
 
-        /**
-         * @var $resource JsonResource
-         */
-        $data = $resource::collection($query->paginate($perPage, $columns, 'page', $page));
-        $paginate = $data->resource;
-        $itens = $data->toArray(new Request(['action' => 'findAll']));
+        $columns = $request->input('columns') ?? ['*'];
+        $page = (int)$request->input('page', 1);
+        $perPage = (int)$request->input('elements', 15);
+
+        /** @var class-string<JsonResource> $resource */
+        $collection = $resource::collection(
+            $query->paginate($perPage, $columns, 'page', $page)
+        );
+
+        $paginate = $collection->resource;
+        $itens = $collection->toArray(new Request([
+            'action' => $action
+        ]));
         return [
             'pagination' => $paginate,
             'itens' => empty($getItens) ? $itens : $getItens($itens)
